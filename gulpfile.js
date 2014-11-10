876e8b80649f158617f42d3881debfc5da26a94c
whwei <whw.0922@gmail.com>
2014-11-10 16:42:02 +0800

    gulp.src & gulp.dest & gulp.task

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['copy-html', 'copy-js', 'copy-css']);
+
+/*
+
+gulp.src(globs[, options])
+从globs读取数据流
+可以通过返回对象的pipe方法写入流到其他插件中
+
+gulp.dest(path[, options])
+写入流到path
+
+gulp.task(name[, deps], fn)
+注册一个task
+
+*/
+
+
+/*
+Glob
+
+ - * matches any number of characters, but not /
+ - ? matches a single character, but not /
+ - ** matches any number of characters, including /, as long as it's the only thing in a path part
+ - {} allows for a comma-separated list of "or" expressions
+ - ! at the beginning of a pattern will negate the match
+
+*/
+
+gulp.task('copy-html', function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy-js', function() {
+  gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-css', function() {
+  gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css/'));
+});
